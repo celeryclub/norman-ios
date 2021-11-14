9f5f3f028c0013cf7fe13c0db06c9a65ddb331d6
Steve Davis <stdavis@adobe.com>
2021-11-13 17:31:47 -0800

    Mock out basic UI

--- Norman/ContentView.swift
+++ Norman/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text("Norman")
+        .font(.title)
+      Text("A cool way to log roast information")
+        .font(.subheadline)
+      Spacer()
+      
+      Button(action: {}) {
+        Text("Begin roast")
+      }
+      HStack{
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+          Text("First crack starts")
+        }
+        Text("- (time)")
+      }
+      HStack {
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+          Text("End roast")
+        }
+        Text("- (time)")
+      }
+      HStack{
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+          Text("First crack ends")
+        }
+        Text("- (time)")
+      }
     }
+    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
