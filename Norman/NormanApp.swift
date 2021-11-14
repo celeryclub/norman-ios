672889e0fda6a08b8641cd4a742a1a6b27fba754
Steve Davis <stdavis@adobe.com>
2021-11-13 17:04:42 -0800

    Initialize Norman app

---
+++ Norman/NormanApp.swift
@@ -0,0 +1,17 @@
+//
+//  NormanApp.swift
+//  Norman
+//
+//  Created by Steve Davis on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct NormanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
